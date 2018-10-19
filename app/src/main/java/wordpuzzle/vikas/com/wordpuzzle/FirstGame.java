0fce084b0d414b6ee8f12e1b7cdf6640da3ad0f7
VikasPandey121 <42648398+VikasPandey121@users.noreply.github.com>
2018-10-19 12:53:55 +0530

    done some new updates but still my TextView problem is not resolved

--- app/src/main/java/wordpuzzle/vikas/com/wordpuzzle/FirstGame.java
+++ app/src/main/java/wordpuzzle/vikas/com/wordpuzzle/FirstGame.java
@@ -2,6 +2,7 @@ package wordpuzzle.vikas.com.wordpuzzle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -14,12 +15,11 @@ public class FirstGame extends AppCompatActivity {
     ImageButton imgbtn;
     Button  E , N , T , U , I;
     TextView txtvww1,txtvw2,txtvw3;
-
-    @Override
+    double j;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_game);
-        int i =0;
+        final int i =0;
         imgbtn = findViewById(R.id.imgbtn);
         E = findViewById(R.id.e);
         N = findViewById(R.id.n);
@@ -31,6 +31,108 @@ public class FirstGame extends AppCompatActivity {
         txtvw3 = findViewById(R.id.txtvw3);
 
 
+        E.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (j==0)
+                {
+                    txtvww1.setText("E");
+                    j++;
+                }
+                else if (j==1)
+                {
+                    txtvw2.setText("E");
+                    j++;
+                }
+                else if (j==2)
+                {
+                    txtvw3.setText("E");
+                    j++;
+                }
+            }
+        });
+
+        N.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (j==0)
+                {
+                    txtvww1.setText("N");
+                    j++;
+                }
+                else if (j==1)
+                {
+                    txtvw2.setText("N");
+                    j++;
+                }
+                else if (j==2)
+                {
+                    txtvw3.setText("N");
+                    j++;
+                }
+            }
+        });
+
+        T.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (j==0)
+                {
+                    txtvww1.setText("T");
+                    j++;
+                }
+                else if (j==1)
+                {
+                    txtvw2.setText("T");
+                    j++;
+                }
+                else if (j==2)
+                {
+                    txtvw3.setText("T");
+                    j++;
+                }
+            }
+        });
+        U.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (j==0)
+                {
+                    txtvww1.setText("U");
+                    j++;
+                }
+                else if (j==1)
+                {
+                    txtvw2.setText("U");
+                j++;
+                }
+                else if (j==2)
+                {
+                    txtvw3.setText("U");
+                    j++;
+                }
+            }
+        });
+        I.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (j==0)
+                {
+                    txtvww1.setText("I");
+                    j++;
+                }
+                else if (j==1)
+                {
+                    txtvw2.setText("I");
+                    j++;
+                }
+                else if (j==2)
+                {
+                    txtvw3.setText("I");
+                    j++;
+                }
+            }
+        });
 
 
 
